75e516557e89655bee20a186cabc7dbababead33
JasperPei <55074462+Jiapeng-Pei@users.noreply.github.com>
2026-09-02 16:34:22 -0700

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string strCleaned = "";
+        for (char c : s) {
+            char cCleaned = cleanChar(c);
+            if (cCleaned != '!') strCleaned.push_back(cCleaned);
+        }
+
+        int left = 0, right = strCleaned.size() - 1;
+        while (left < right) {
+            if (strCleaned[left++] != strCleaned[right--]) return false; 
+        }
+
+        return true;
+    }
+
+private: 
+
+    char cleanChar(char c) {
+        bool isUpperLetter = c >= 'A' && c <= 'Z';
+        bool isLowerLetter = c >= 'a' && c <= 'z';
+        bool isNumber = c >= '0' && c <= '9';
+
+        if (!isUpperLetter && !isLowerLetter && !isNumber) {
+            return '!';
+        }
+
+        if (isUpperLetter) {
+            return u_char('a' + c - 'A');
+        }
+        else {
+            return c;
+        }
+    }
+};
