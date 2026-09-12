1b718be707c6470c0479ba79726e61b253fa252b
JasperPei <55074462+Jiapeng-Pei@users.noreply.github.com>
2026-09-12 16:22:14 -0700

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.cpp
@@ -0,0 +1,40 @@
+/*
+给定一个速度K，我应该如何计算出需要多久把香蕉吃完呢？时间复杂度是多少？O(n)
+可以binary search k，下界是0，上节是max(piles)
+*/
+
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int right = 0;
+        for (int n : piles) right = max(right, n);
+
+        int left = 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int hours = hoursNeeded(piles, mid);
+            
+            // not enough time. Need to increase the speed.
+            if (hours > h) {
+                left = mid + 1;
+            }
+            // enough time. Keep this possible speed and reduce the window size.
+            else {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+
+private: 
+    int hoursNeeded(vector<int>& piles, int k) {
+        int ret = 0;
+        for (int n : piles) {
+            ret += n / k;
+            ret += n % k ? 1 : 0;
+        }
+
+        return ret;
+    }
+};
